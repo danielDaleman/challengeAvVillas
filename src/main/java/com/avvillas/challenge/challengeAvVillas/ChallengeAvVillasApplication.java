79f0961659d22e7ff85b0ab5f28c4aa3415d6ac7
dfmoreno <dfmoreno@asesoftware.com>
2021-03-27 22:49:09 -0500

    Base proyecto

---
+++ src/main/java/com/avvillas/challenge/challengeAvVillas/ChallengeAvVillasApplication.java
@@ -0,0 +1,13 @@
+package com.avvillas.challenge.challengeAvVillas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAvVillasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeAvVillasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avvillas/challenge/challengeAvVillas/ChallengeAvVillasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avvillas.challenge.challengeAvVillas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAvVillasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
